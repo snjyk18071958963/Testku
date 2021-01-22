6d642b1616b9b90bdcc471b327088c201ab84f23
jianhong <1685940190@qq.com>
2021-01-22 16:16:29 +0800

    test conflict left

--- src/main/java/com/example/pojo/Apple.java
+++ src/main/java/com/example/pojo/Apple.java
@@ -6,6 +6,7 @@ public class Apple {
 		// TODO Auto-generated method stub
 			System.out.println("杨键弘 第二次测试");
 			System.out.println("重要测试");
+			System.out.println("杨键弘 第一个项目   做上传 left");
 	}
 
 }
